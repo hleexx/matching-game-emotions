e7cab592b6fa748162c4a176c9e1856e80db6ffd
Hellen Lee <hleexx@gmail.com>
2018-05-10 00:08:23 -0400

    Testing timer code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 $(function)() {
 	
+	// function for timer
+
+	// let interval = setInterval(function(){ myTimer() }, 1000);
+
+	// function myTimer() {
+	// 	var s = new Date();
+	// 	var t = d.toLocaleTimeString();
+	// 	document.getElementbyId("timer").innerHTML = t;
+	// }
+
+	// let seconds = 0;
+	// let minutes = 0;
+
+	// function startTime() {
+	// 	var m = minutes++
+	// 	var s = seconds++
+	// 	document.getElementsbyClassName("timer").innerHTML = ${m} : ${s};
+	// 	var t = setTimeout(function(){startTimer()}, 500);
+	// }
+
+	let seconds = 0;
+
+	document.getElementsbyClassName("timer").innerHTML = seconds;
+	
 }
